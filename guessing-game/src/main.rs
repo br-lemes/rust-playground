72b9f3f12220fc64f74056d31cdeb06091a478e1
Breno Ramalho Lemes <breno@br-lemes.net>
2022-09-04 23:15:44 -0400

    Third step

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -10,5 +11,11 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+    let guess: u8 = guess.trim().parse().expect("Please type a number!");
     println!("You guessed: {}", guess);
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
